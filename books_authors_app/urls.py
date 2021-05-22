f32aa9913ab6e46226945e561e54f8089fcbfe5f
oassami <76979230+oassami@users.noreply.github.com>
2021-05-21 18:17:05 -0700

    Assignment Submitted

--- books_authors_app/urls.py
+++ books_authors_app/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('add_book', views.add_book),
+    path('add_author', views.add_author),
+    path('book/<int:book_id>', views.view_book),
+    path('author/<int:author_id>', views.view_author),
+    path('add_auth_book/<int:book_id>', views.add_auth_book),
+    path('authors', views.authors),
+    path('author/<int:author_id>', views.view_author),
+    path('add_book_auth/<int:author_id>', views.add_book_auth),
 ]
